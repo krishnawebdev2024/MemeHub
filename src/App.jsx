75dc80bf2947eb6c1be17f4c2ee2a0aefabaa719
krishna <krisch.webdev@gmail.com>
2024-10-09 12:37:18 +0200

    Boiler-plate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Nav from "./components/NavBar";
+import Footer from "./components/Footer";
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <Home />
+      <Gallery />
+      <Footer />
+    </>
+  );
+}
